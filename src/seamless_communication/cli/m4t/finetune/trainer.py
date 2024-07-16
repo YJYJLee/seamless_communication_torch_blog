cc51250552def1cac0bb09bcd839bd77850fca67
Mostafa Elhoushi <melhoushi@meta.com>
2024-07-16 02:49:40 +0000

    get early exit loss to work

--- src/seamless_communication/cli/m4t/finetune/trainer.py
+++ src/seamless_communication/cli/m4t/finetune/trainer.py
@@ -22,6 +22,7 @@ from fairseq2.models.sequence import SequenceModelOutput
 from fairseq2.nn.padding import PaddingMask
 from fairseq2.optim.lr_scheduler import MyleLR
 from fairseq2.typing import Device
+from fairseq2.utils.early_exit_loss import early_exit_loss, hidden_states_dict
 from torch.optim import AdamW
 
 from seamless_communication.cli.m4t.finetune import dataloader, dist_utils
@@ -376,8 +377,11 @@ class UnitYFinetune:
         self.optimizer.zero_grad()
         with torch.autocast(device_type=self.params.device.type, dtype=self.params.float_dtype):
             tokens, units = self.model(batch)
-        
-        loss = self.calc_loss(batch, tokens, units)
+            if False:
+                loss = self.calc_loss(batch, tokens, units)
+            else:
+                (_, _) = self.model(batch)
+                loss = early_exit_loss(self.model, hidden_states_dict, batch, self.calc_loss, e_scale=1.0, loss_scale_type="sum_l")
         if loss.isnan().any().item():
             logger.error(batch.speech_to_text)
             raise RuntimeError("Train loss is NaN! Something is wrong in the model!")
